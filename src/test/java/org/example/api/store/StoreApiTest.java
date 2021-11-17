51a8edc9de9b8c2a0ab30114ec3c2c2c6dc85d72
Basile <interbeing34@gmail.com>
2021-11-17 21:48:13 +0300

    develop commit

--- src/test/java/org/example/api/store/StoreApiTest.java
+++ src/test/java/org/example/api/store/StoreApiTest.java
@@ -1,69 +1,124 @@
 package org.example.api.store;
 
+
+
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
-import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.testng.Assert;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.responseSpecification;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class StoreApiTest {
 
+    private Order order;
+    private int id;
 
-    @Test
-    public void placeOrderTest (){
-        String createPet = "{\n" +
-                "  \"id\": 12074,\n" +
-                "  \"petId\": 8080,\n" +
-                "  \"quantity\": 3,\n" +
-                "  \"shipDate\": \"2021-11-14T16:12:09.441Z\",\n" +
-                "  \"status\": \"placed\",\n" +
-                "  \"complete\": true\n" +
-                "}";
+    @BeforeClass
+    public void prepare() throws IOException {
+        order = new Order();
+        id = new Random().nextInt(5000);
+        int petId = new Random().nextInt(3000);
+        int quantity = new Random().nextInt(50);
+        order.setId(id);
+        order.setPetId(petId);
+        order.setQuantity(quantity);
+        order.setShipDate("2021-05-26T19:53:29.159Z");
+        order.setStatus("placed");
+        order.setComplete(true);
+
+        System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://petstore.swagger.io/v2/")
+                .addHeader("api_key", System.getProperty("api.key"))
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+
+        RestAssured.filters(new ResponseLoggingFilter());
+
         given()
-                .header("Content-Type", "application/json")
-                .accept(ContentType.JSON)
-                .body(createPet)
+                .body(order)
                 .when()
-                .post("https://petstore.swagger.io/v2/store/order")
-                .prettyPeek()
+                .post("/store/order")
                 .then()
                 .statusCode(200);
     }
 
     @Test
-    public void deleteOrderTest() {
-        // todo: удалить заказ
-        // todo: проверить удаление заказа
-        given()
-                .header("Authorization", "special-key")
-                .log()
-                .all()
-                .when()
-                .delete("https://petstore.swagger.io/v2/store/order/12074")
-                .prettyPeek()
-                .then()
-                .statusCode(200)
-                .and()
-                .body("type", CoreMatchers.is("unknown"))  //проверяем, что вместо заказа стало "unknown"
-                .log()
-                .all();
-        // .body("message", CoreMatchers.is("Order Not Found"));
+    public void createOrder() {
+
+        Order current =
+                given()
+                        .pathParam("id", id)
+                        .when()
+                        .get("/store/order/{id}")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Order.class);
+
+        assertEquals(current.getId(), order.getId());
     }
 
-}
+    @Test
+    public void getOrder() {
 
+        Order current =
+                given()
+                        .pathParam("id", order.getId())
+                        .when()
+                        .get("/store/order/{id}")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Order.class);
+
+        assertEquals(current.getId(), order.getId());
+    }
+
+    @Test
+    public void orderDelete() {
+
+        given()
+                .pathParam("id", order.getId())
+                .when()
+                .delete("/store/order/{id}")
+                .then()
+                .statusCode(200);
+        given()
+                .pathParam("id", order.getId())
+                .when()
+                .get("/store/order/{id}")
+                .then()
+                .statusCode(404);
+    }
+
+
+    @Test
+    public void tryWithMap() {
+        Map inventory =
+                given()
+                        .when()
+                        .get("/store/inventory")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Map.class);
+        assertTrue(inventory.containsKey("sold"), "нет статуса sold");
+    }
+}
